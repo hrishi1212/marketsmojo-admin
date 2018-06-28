73db5c5baabacf0276802644ac965ea755e27ccf
hrishikesh1212 <hrishikesh@marketsmojo.com>
2018-06-28 14:59:40 +0530

    admin changes

--- src/app/service/url.service.ts
+++ src/app/service/url.service.ts
@@ -1,3 +1,7 @@
-export const LOGIN_REST_URL : string = "http://marketmojo.com/mojo/";
-export const FRAPI_URL :string = "http://frapi.marketmojo.com";
-export const CMS_URL :string = "http://cms.marketmojo.com";
+export const LOGIN_REST_URL : string = "https://sandbox-www.marketsmojo.com/mojo/";
+export const FRAPI_URL :string = "https://sandbox-frapi.marketsmojo.com";
+export const CMS_URL :string = "https://sandbox-cms.marketsmojo.com";
+
+// export const LOGIN_REST_URL : string = "http://www.marketmojo.com/mojo/";
+// export const FRAPI_URL :string = "http://frapi.marketmojo.com";
+// export const CMS_URL :string = "http://cms.marketmojo.com";
