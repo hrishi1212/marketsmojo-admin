96c40de1ff4fb556e1feb20566f3222d58b4e6ef
hrishikesh1212 <hrishikesh@marketsmojo.com>
2018-08-02 18:07:26 +0530

    utmsource changes

--- src/app/service/url.service.ts
+++ src/app/service/url.service.ts
@@ -3,14 +3,14 @@
 // export const CMS_URL :string = "https://sandbox-cms.marketsmojo.com";
 // export const FRONTEND_URL : string = "https://sandbox-www.marketsmojo.com/";
 
-export const LOGIN_REST_URL : string = "http://marketmojo.com/mojo/";
-export const FRONTEND_URL : string = "http://www.marketmojo.com/";
-export const FRAPI_URL :string = "http://frapi.marketmojo.com";
-export const CMS_URL :string = "http://cms.marketmojo.com";
+// export const LOGIN_REST_URL : string = "http://marketmojo.com/mojo/";
+// export const FRONTEND_URL : string = "http://www.marketmojo.com/";
+// export const FRAPI_URL :string = "http://frapi.marketmojo.com";
+// export const CMS_URL :string = "http://cms.marketmojo.com";
 
-// export const LOGIN_REST_URL : string = "https://www.marketsmojo.com/mojo/";
-// export const FRAPI_URL :string = "https://frapi.marketsmojo.com";
-// export const CMS_URL :string = "https://cms.marketsmojo.com";
-// export const FRONTEND_URL : string = "https://www.marketsmojo.com/";
+export const LOGIN_REST_URL : string = "https://www.marketsmojo.com/mojo/";
+export const FRAPI_URL :string = "https://frapi.marketsmojo.com";
+export const CMS_URL :string = "https://cms.marketsmojo.com";
+export const FRONTEND_URL : string = "https://www.marketsmojo.com/";
 
 
