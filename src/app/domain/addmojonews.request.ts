943cb48d3a4cbc7595f78ba9af3627b302bd233b
Surajit <surajit@marketsmojo.com>
2018-09-12 16:20:02 +0530

    added domain request

---
+++ src/app/domain/addmojonews.request.ts
@@ -0,0 +1,13 @@
+export class AddMojoNews{
+    newsid:number;
+    title:string;
+    imagepath:string;
+    link:string;
+    source:string;
+    publisheddate:Date;
+    description:string;
+    newstype:string;
+    isdeleted:number;
+    mapstock: any[];
+    topnews:string = '0';
+}
